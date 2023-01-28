82c539177cb6642d4986634431d99c5796571f1a
satya mishra <satyamishra2007@gmail.com>
2023-01-28 12:52:21 -0800

    test

--- basic/loops/for_loop.py
+++ basic/loops/for_loop.py
@@ -1,13 +1,12 @@
-arr = [1,3,5,8,10]
-l = len(arr)
-for i in arr:
-    print(i)
+arr = [1, 3, 5, 8, 10]
+lgt = len(arr)
+
+for num in arr:
+    print(num)
 
 
-for i in range(l):
+for i in range(lgt):
     print(arr[i])
 
 
 
-
-list1 = []
